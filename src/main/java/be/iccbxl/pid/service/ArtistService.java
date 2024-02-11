7b17f277b2c69a90af1cd68649830374ff9d6107
Soufiane MEFTAH <s.mef2703@gmail.com>
2024-02-11 12:59:10 +0100

    Modèle Artist +migration +données initiales

---
+++ src/main/java/be/iccbxl/pid/service/ArtistService.java
@@ -0,0 +1,42 @@
+package be.iccbxl.pid.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import be.iccbxl.pid.models.Artist;
+import be.iccbxl.pid.repository.ArtistRepository;
+
+@Service
+public class ArtistService {
+	@Autowired
+	private ArtistRepository artistRepository;
+		
+	public List<Artist> getAllArtists() {
+		List<Artist> artists = new ArrayList<>();
+		
+		artistRepository.findAll().forEach(artists::add);
+		
+		return artists;
+	}
+	
+	public Artist getArtist(int id) {
+		return artistRepository.findById(id);
+	}
+ 
+	public void addArtist(Artist artist) {
+		artistRepository.save(artist);
+	}
+ 
+	public void updateArtist(int id, Artist artist) {
+		artistRepository.save(artist);
+	}
+ 
+	public void deleteArtist(int id) {
+		artistRepository.deleteById((long) id);
+	}
+
+}
+

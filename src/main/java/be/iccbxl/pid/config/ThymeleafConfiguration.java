f2f0ea411ef40f0ecac105596ebfced61221ca68
Soufiane MEFTAH <s.mef2703@gmail.com>
2024-03-01 12:02:49 +0100

    Controller Artist +templates +routes

--- src/main/java/be/iccbxl/pid/config/ThymeleafConfiguration.java
+++ src/main/java/be/iccbxl/pid/config/ThymeleafConfiguration.java
@@ -1,15 +1,19 @@
 package be.iccbxl.pid.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
+
 @Configuration
 public class ThymeleafConfiguration {
-    @Bean
-    public LayoutDialect thymeleafDialect() {
+	@Bean
+	public LayoutDialect thymeleafDialect() {
 	    return new LayoutDialect();
 	}
 }
+
+
 

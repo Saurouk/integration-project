f716a6696248dee1c8c008c125f699ce799c120b
Soufiane MEFTAH <s.mef2703@gmail.com>
2024-03-02 22:07:33 +0100

    Relation ManyToMany between User & Role

--- src/main/java/be/iccbxl/pid/models/Role.java
+++ src/main/java/be/iccbxl/pid/models/Role.java
@@ -1,30 +1,43 @@
 package be.iccbxl.pid.models;
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name="roles")
 public class Role {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String role;
 	
-	protected Role() {	}
+	@ManyToMany
+	@JoinTable(
+		  name = "user_role", 
+		  joinColumns = @JoinColumn(name = "role_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users = new ArrayList<>();
+	
+	protected Role() { }
 	
 	public Role(String role) {
-		super();
 		this.role = role;
 	}
 
 	public int getId() {
 		return id;
 	}
-
+	
 	public String getRole() {
 		return role;
 	}
@@ -33,11 +46,34 @@ public class Role {
 		this.role = role;
 	}
 
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public Role addUser(User user) {
+		if(!this.users.contains(user)) {
+			this.users.add(user);
+			user.addRole(this);
+		}
+		
+		return this;
+	}
+	
+	public Role removeUser(User user) {
+		if(this.users.contains(user)) {
+			this.users.remove(user);
+			user.getRoles().remove(this);
+		}
+		
+		return this;
+	}
+
 	@Override
 	public String toString() {
 		return "Role [id=" + id + ", role=" + role + "]";
 	}
 	
 }
+
 
 

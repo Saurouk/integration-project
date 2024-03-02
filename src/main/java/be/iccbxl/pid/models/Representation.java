f8e1b5e0e66ab86d75fc04641562a80776828f92
Soufiane MEFTAH <s.mef2703@gmail.com>
2024-03-02 22:21:23 +0100

    Relation ManyToMany between User & Representation

--- src/main/java/be/iccbxl/pid/models/Representation.java
+++ src/main/java/be/iccbxl/pid/models/Representation.java
@@ -1,12 +1,16 @@
 package be.iccbxl.pid.models;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -32,6 +36,14 @@ public class Representation {
 	@ManyToOne
 	@JoinColumn(name="location_id", nullable=true)
 	private Location location;
+	
+	@ManyToMany
+	@JoinTable(
+		  name = "reservations", 
+		  joinColumns = @JoinColumn(name = "representation_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users = new ArrayList<>();
+
 
 	public Representation() { }
 	
@@ -68,6 +80,31 @@ public class Representation {
 	public int getId() {
 		return id;
 	}
+	
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public Representation addUser(User user) {
+		if(!this.users.contains(user)) {
+			this.users.add(user);
+			user.addRepresentation(this);
+		}
+		
+		return this;
+	}
+	
+	public Representation removeUser(User user) {
+		if(this.users.contains(user)) {
+			this.users.remove(user);
+			user.getRepresentations().remove(this);
+		}
+		
+		return this;
+	}
+
+	
+	
 
 	@Override
 	public String toString() {
--- src/main/java/be/iccbxl/pid/models/User.java
+++ src/main/java/be/iccbxl/pid/models/User.java
@@ -27,6 +27,9 @@ private String email;
 	
 	@ManyToMany(mappedBy = "users")
 	private List<Role> roles = new ArrayList<>();
+	@ManyToMany(mappedBy = "users")
+	private List<Representation> representations = new ArrayList<>();
+
 
 	protected User() {}
 
@@ -114,6 +117,29 @@ private String email;
 		
 		return this;
 	}
+	
+	public List<Representation> getRepresentations() {
+		return representations;
+	}
+
+public User addRepresentation(Representation representation) {
+		if(!this.representations.contains(representation)) {
+			this.representations.add(representation);
+			representation.addUser(this);
+		}
+		
+		return this;
+	}
+	
+	public User removeRepresentation(Representation representation) {
+		if(this.representations.contains(representation)) {
+			this.representations.remove(representation);
+			representation.getUsers().remove(this);
+		}
+		
+		return this;
+	}
+
 
 	@Override
 	public String toString() {
